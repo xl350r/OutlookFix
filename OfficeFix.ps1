6331a5f1a90e41b003dfd7ad8f9b89d1ff893466
xl350r <danhsecure@gmail.com>
2020-05-06 07:51:03 -0700

    Powershell alternative added.

---
+++ OfficeFix.ps1
@@ -0,0 +1,8 @@
+﻿if ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+    if ($(New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Identity").GetValue("EnableADAL") -eq $null) {
+        New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Identity" -name EnableADAL -value 1  
+    }
+    if ($(New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Identity").GetValue("Version") -eq $null) {
+        New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Identity" -name Version -Value 1
+    }
+}
